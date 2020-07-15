74d22ae73d0f2d714b95776ac95ba97966a495ed
PraveenS0906 <66065738+PraveenS0906@users.noreply.github.com>
2020-07-15 16:50:27 -0700

    module2

--- app.js
+++ app.js
@@ -1,32 +1,75 @@
-(function(){
-    angular.module("LunchCheck", [])
-    .controller("LunchCheckController", myfood);
+(function (){
 
-    myfood.$inject=["$scope"];
-    function myfood($scope){
-        $scope.food='';
-        $scope.checkfood=function(){
-            
-            $scope.lunchmessage=findmessage($scope.food.split(","));
+    'use strict';
 
-            function findmessage(string){
-            if(string=="")
-            {
-                return "Please Enter The Data First";
-            }
-            else{
-                if(string.length<=3)
-                {
-                    return "Enjoy!";
-                }
-                else
-                {
-                    return "Too Much!";
-                }
-            }
-            
-            
+    angular.module("ShoppingListCheckoff", [])
+    .controller("ToBuyController", ToBuyController)
+    .controller("AlreadyBoughtController", AlreadyBoughtController)
+    .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+    ToBuyController.$inject=["ShoppingListCheckOffService"];
+    function ToBuyController(ShoppingListCheckOffService){
+
+        var tobuy=this;
+        
+        tobuy.items=ShoppingListCheckOffService.showitems();
+
+        tobuy.removeitem=function(itemIndex){
+            ShoppingListCheckOffService.removeitem(itemIndex);
         };
-        }
     }
+
+    AlreadyBoughtController.$inject=["ShoppingListCheckOffService"];
+    function AlreadyBoughtController(ShoppingListCheckOffService){
+            var bought=this;
+
+            bought.newitems=ShoppingListCheckOffService.additem();
+    }
+
+    function ShoppingListCheckOffService(){
+            var service=this;
+
+            var items=[
+                {
+                    name:"Chocolate",
+                    quantity:3
+                },
+                {
+                    name:"Chips",
+                    quantity:10
+                },
+                {
+                    name:"Pizza",
+                    quantity:2
+                },
+                {
+                    name:"Burger",
+                    quantity:6
+                },
+                {
+                    name:"Ice Cream",
+                    quantity:20
+                }
+            ];
+
+            var newitems=[];
+            
+            service.removeitem=function(itemIndex){
+                newitems.push(items[itemIndex]);
+                items.splice(itemIndex, 1);
+                
+            };
+            
+            service.additem=function(itemIndex){
+                
+                return newitems;
+            };
+            service.showitems=function(){
+                return items;
+            };
+            
+            
+    }
+
+
 })();
